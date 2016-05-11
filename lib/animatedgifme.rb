2315310473436914743157fbbe6af11be7ad6965
shakycode <jelinek@gmail.com>
2016-05-11 13:21:20 -0500

    Fix problems with private method

--- lib/animatedgifme.rb
+++ lib/animatedgifme.rb
@@ -6,20 +6,14 @@ module Animatedgifme
   base_uri "animatedgif.me"
 
   def self.find(id)
-    retrieve_url get("/gifs/#{id}.json")
+    get("/gifs/#{id}.json").parsed_response["url"]
   end
 
   def self.tagged(tag)
-    retrieve_url get("/#{tag}.json")
+    get("/#{tag}.json").parsed_response["url"]
   end
 
   def self.random(tag)
     tagged(tag)
   end
-
-  private
-
-    def retrieve_url(response)
-      response.parsed_response["url"]
-    end
 end
